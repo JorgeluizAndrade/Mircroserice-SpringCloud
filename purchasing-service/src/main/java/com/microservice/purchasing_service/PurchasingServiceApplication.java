fb66a9b41a3c4c57c04fb9c5a84e7c2baf890b33
jorge luiz <luizj1718@gmail.com>
2025-01-23 12:19:17 -0300

    style

--- purchasing-service/src/main/java/com/microservice/purchasing_service/PurchasingServiceApplication.java
+++ purchasing-service/src/main/java/com/microservice/purchasing_service/PurchasingServiceApplication.java
@@ -1,4 +1,4 @@
-package com.micriservice.purchasing_service;
+package com.microservice.purchasing_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class PurchasingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PurchasingServiceApplication.class, args);
 	}
-
 }
